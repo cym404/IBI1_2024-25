165308368dc9caddafa35d2f5cdaf70c1abdcd71
cym <yimeng.24@intl.zju.edu.cn>
2025-03-19 14:45:14 +0800

    update practical5

---
+++ practical5/1.py
@@ -0,0 +1,32 @@
+#(1. Create and print a dictionary
+#DECLARE language_percentage AS DICTIONARY
+#language_percentage["JavaScript"] = 62.3
+#language_percentage["HTML"] = 52.9
+#language_percentage["Python"] = 51
+#language_percentage["SQL"] = 46
+#language_percentage["TypeScript"] = 38.5
+#PRINT language_percentage
+#2. Construct a bar plot
+#// Assume we have a function to create bar plot, like createBarPlot(x_data, y_data)
+#DECLARE languages AS ARRAY = ["JavaScript", "HTML", "Python", "SQL", "TypeScript"]
+#DECLARE percentages AS ARRAY = [62.3, 52.9, 51, 46, 38.5]
+#CALL createBarPlot(languages, percentages)
+#3. Get the percentage of a specified language
+#DECLARE input_language AS STRING // Assume it's assigned a value, e.g., "Python"
+#DECLARE percentage AS FLOAT
+#SET percentage = language_percentage[input_language] IF input_language IN language_percentage ELSE "Language not found"
+#PRINT percentage）
+
+
+language_percentage={"javaScript":62.3,"HTML":52.9,"Python":51,"SQL":46,"TypeScript":38.5}
+import matplotlib.pyplot as plt
+languages=list(language_percentage.keys())
+percentages=list(language_percentage.values())
+plt.bar(languages,percentages)
+plt.xlabel("Programming Languages")
+plt.ylabel("Precentage of Users")
+plt.title("popularity of top 5 programming languages")
+plt.show()
+input_language="Python"#input Python as the selected language
+percentage=language_percentage.get(input_language)
+print("python",percentage)
---
+++ practical5/2.py
@@ -0,0 +1,43 @@
+#1. Sort and print the population lists
+#// Population of UK's constituent countries
+#DECLARE uk_population AS ARRAY = [57.11, 3.13, 1.91, 5.45]
+#SORT uk_population IN ASCENDING ORDER
+#PRINT "UK's constituent countries sorted population list: ", uk_population
+
+#// Population of provinces around Zhejiang Province
+#DECLARE zj_neighbor_population AS ARRAY = [65.77, 41.88, 41.41, 61.27, 61.15]
+#SORT zj_neighbor_population IN ASCENDING ORDER
+#PRINT "Provinces around Zhejiang Province sorted population list: ", zj_neighbor_population
+#2. Draw pie charts
+#// Assume there is a function to draw pie charts, like drawPieChart(labels, data)
+
+#// For the UK's constituent countries
+#DECLARE uk_labels AS ARRAY = ["England", "Wales", "Northern Ireland", "Scotland"]
+#DECLARE uk_population_data AS ARRAY = [57.11, 3.13, 1.91, 5.45]
+#CALL drawPieChart(uk_labels, uk_population_data)
+#PRINT "Population distribution pie chart of UK's constituent countries"
+
+#// For provinces around Zhejiang Province
+#DECLARE zj_labels AS ARRAY = ["Fujian", "Jiangxi", "Anhui", "Jiangsu"]
+#DECLARE zj_neighbor_population_data AS ARRAY = [65.77, 41.88, 41.41, 61.27, 61.15]
+#CALL drawPieChart(zj_labels, zj_neighbor_population_data)
+#PRINT "Population distribution pie chart of provinces around Zhejiang Province"
+uk_population=[57.11,3.13,1.91,5.45]
+uk_population.sort()
+zj_neighbor_population=[65.77,41.88,41.41,61.27,61.25]
+zj_neighbor_population.sort()
+print("	populations	of	countries	in	the	UK",uk_population)
+print("population of Zhejiang-neighbouring	provinces	in	China",zj_neighbor_population)
+import matplotlib.pyplot as plt
+uk_labels=["England","Wales","Northern	Ireland","Scotland"]
+uk_population=[57.11,3.13,1.91,5.45]
+zj_labels=["Zhejiang","Fujian","Jiangxi","Anhui","Jiangsu"]
+zj_neighbor_population=[65.77,41.88,41.41,61.27,61.25]
+plt.figure(figsize=(6, 6))
+plt.pie(uk_population, labels=uk_labels, autopct='%1.1f%%', colors=["r","g","b","y"])#make a pie map for uk population, and change the color of each country
+plt.title('Population Distribution in UK Countries')
+plt.show()
+plt.figure(figsize=(6, 6))
+plt.pie(zj_neighbor_population, labels=zj_labels, autopct='%1.1f%%', startangle=140)#make a pie map for Population Distribution in Zhejiang - Neighboring Provinces, and change the starting angle of the provinces
+plt.title('Population Distribution in Zhejiang - Neighboring Provinces')
+plt.show()
